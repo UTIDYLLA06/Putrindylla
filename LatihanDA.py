62eef960af04ea1992c0a65dcbaec94f7630f5f0
UTIDYLLA06 <140218764+UTIDYLLA06@users.noreply.github.com>
2023-08-19 07:27:56 +0700

    Add files via upload

---
+++ LatihanDA.py
@@ -0,0 +1,4 @@
+print("Nama:Putri Nadylla")
+print("Kelas:X-RPL1")
+print("Alasan masuk RPL: alasan saya masuk rpl karena saya ingin mengetahui serta mempelajari ilmu komputer,dan saya bercita-cita sebagai pengusaha ")
+
